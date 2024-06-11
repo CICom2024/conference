ae7f584e9c72a9d7719da49cb31b90c758a64bce
bikrampratimbhuyan <bikram23bhuyan@gmail.com>
2024-06-11 17:22:34 +0200

    first commit

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -2,19 +2,20 @@ import PropTypes from "prop-types";
 import ServiceCard from "../ServiceCard/ServiceCard";
 import {useContext} from "react";
 import {ServiceContext} from "../../context/ServiceProvider";
+import logo1 from "../../assets/images/logo-lisv.jpg";
+import logo2 from "../../assets/images/lyrids.png";
 
 const Services = () => {
   const services = useContext(ServiceContext);
   return (
     <section className="min-h-[30rem] bg-[#181A1B]  p-5 md:p-10 lg:p-20">
       <h1 className="font-bold capitalize text-xl  md:text-3xl lg:text-5xl text-white mb-10" data-aos="fade-up">
-        our services
+        Organising Laboratories
       </h1>
       <div className="grid grid-cols-1 lg:grid-cols-3 place-items-center lg:place-items-start gap-10">
-        {services.map((service) => (
-          <ServiceCard key={service?.id} service={service} />
-        ))}
+        
       </div>
+      
     </section>
   );
 };
